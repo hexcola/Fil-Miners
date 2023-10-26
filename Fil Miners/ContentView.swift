0d10e01302f4cae255a64352df011635c06aceeb
hexcola <hexcola@gmail.com>
2023-10-26 21:42:35 +0800

    Initial Commit

---
+++ Fil Miners/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fil Miners
+//
+//  Created by Neo on 2023/10/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fil Miners/Fil_MinersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fil_MinersApp.swift
+//  Fil Miners
+//
+//  Created by Neo on 2023/10/26.
+//
+
+import SwiftUI
+
+@main
+struct Fil_MinersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
